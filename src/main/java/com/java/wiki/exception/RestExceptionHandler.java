1793fb11f7bb677f0d855678bcb295a721d22cec
senzou-kevin <825484312@qq.com>
2023-01-19 09:25:43 +0100

    [ADD] exception handler for validation check.

---
+++ src/main/java/com/java/wiki/exception/RestExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.java.wiki.exception;
+
+import com.java.wiki.commons.ResultCode;
+import com.java.wiki.resp.CommonResp;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+    /**
+     * catch and return if the validation check has errors.
+     * @return
+     */
+    @ExceptionHandler({BindException.class})
+    public CommonResp methodArgumentExceptionHandler(){
+        return CommonResp.fail(ResultCode.FAIL.getMessage(), ResultCode.FAIL.getCode());
+    }
+}
